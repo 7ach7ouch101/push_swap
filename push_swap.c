5d214437f89a4da3316d95ca9ff3d5f691d54e9f
7ach7ouch101 <69723977+7ach7ouch101@users.noreply.github.com>
2022-05-10 16:57:49 -0700

    Add files via upload

--- push_swap.c
+++ push_swap.c
@@ -455,31 +455,64 @@ int    parse(int ac, char **av,struct Node **a)
     }
     return 1;
 }
-/*int is_sorted(struct Node *a)
+int is_sorted(struct Node *a)
 {
-    struct Node *tmp;
+    int data;
 
-    tmp = a;
+    data = a->data;
     a = a->next;
-    while(tmp < a->data)
+    while(data < a->data)
     {
-        if(a->next == NULL)
-        {
-
-        }
+        data = a->data;
+        if(a->next->next == NULL && data < a->next->data)
+            return 1;
         a = a->next;
     }
     return 0;
 }
+int	small_instack(struct Node *a)
+{
+	int	tmp;
+
+	tmp = (a)->data;
+	while (a)
+	{
+		if (tmp > (a)->data)
+			tmp = (a)->data;
+		a = (a)->next;
+	}
+	return (tmp);
+}
 void    sort_five(struct Node **a, struct Node **b)
 {
-    pb(&*(a),&*(b));
-    pb(&*(a),&*(b));
-    sort_three(a);
-    while(!is_sorted(a))
-    {
-    }
-}*/
+	while (*a)
+	{
+		if (lstsize(*b) == 2)
+			break ;
+		if ((*a)->data == small_instack(*a))
+			pb(&*(a),&*(b));
+		else
+			ra(&*(a));
+	}
+	while (lstsize(*a) == 3)
+	{
+		if ((*a)->data > (*a)->next->next->data)
+			ra(&*(a));
+		else if ((*a)->data > (*a)->next->data)
+			sa(*(a));
+		else if ((*a)->next->data > (*a)->next->next->data)
+			rra(&*(a));
+		else
+			break ;
+	}
+	while (*b)
+		pa(&*(a),&*(b));
+}
+void    sort_two(struct Node **a)
+{
+    if((*a)->data > (*a)->next->data)
+        sa(*(a));
+}
 int main(int ac, char **av)
 {
     struct Node *a;
@@ -487,16 +520,17 @@ int main(int ac, char **av)
 
     b = NULL;
     a = NULL;
-    if(ac <= 2 || parse(ac,av,&a) == 0)
+    if(ac <= 2 || parse(ac,av,&a) == 0 || is_sorted(a))
         return 0;
-    /*if(lstsize(a) == 3)
+    if(lstsize(a) == 2)
+        sort_two(&a);
+    else if(lstsize(a) == 3)
         sort_three(&a);
     else if(lstsize(a) == 5)
         sort_five(&a,&b);
-    else*/
-
-    above_five(&a,&b);
-    //printnode(a, 'a');
+    else
+        above_five(&a,&b);
+    printnode(a, 'a');
     freelist(&a,&b);
     return 0;
 }
